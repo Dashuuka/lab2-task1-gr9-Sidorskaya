d2f69077d742a2137399dee3fcd105c9048af06f
Darya Sidorskaya <164616714+Dashuuka@users.noreply.github.com>
2025-03-01 16:18:17 +0000

    Add sum

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 /* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main (void)
+int sum_of_digits(int number) {
+		number = abs(number);
+		int sum = 0;
+		while (number > 0) {
+				sum += number % 10;
+				number /= 10;
+		}
+		return sum;
+}
+
+
+int main(int argc, char *argv[])
 {
 	printf ("Hello World\n");
 	printf ("Goodbye World\n");
